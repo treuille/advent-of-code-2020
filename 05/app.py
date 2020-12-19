acb041bee6b2411b0fa1a7596c3af4e752ac23a4
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-12-19 01:01:31 -0800

    Finished part 1 of 05

---
+++ 05/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from streamlit_ace import st_ace
+
+"""
+# Advent of Code 2020 in Streamlit - 05
+"""
+
+"""
+## Input
+"""
+
+# Load the input
+problem_input = st_ace(height=150)
+show_debug_output = st.checkbox("Show debug output")
+
+"""
+## Output
+"""
+
+# BFFFBBFRRR: row 70, column 7, seat ID 567.
+# FFFBBBFRRR: row 14, column 7, seat ID 119.
+# BBFFBBFRLL: row 102, column 4, seat ID 820.
+
+max_seat = -1
+for line_num, line in enumerate(problem_input.split('\n')):
+    row_str = line[:7].replace('B', '1').replace('F', '0')
+    row = int(row_str, 2)
+    col_str = line[7:10].replace('R', '1').replace('L', '0')
+    col = int(col_str, 2)
+    seat = row * 8 + col
+    if show_debug_output:
+        st.write(f'`{line_num}`: line=`"{line}"`')
+        st.write(col_str, col)
+        st.write(row_str, row)
+        st.write("seat:", seat)
+    if seat > max_seat:
+        max_seat = seat
+st.write("Highest seat:", max_seat)
