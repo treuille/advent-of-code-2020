ec3b9dad5a700fbb2d20784b1cb69f92f5e2b974
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-12-18 22:02:24 -0800

    First half of problem 2 solved

--- 02/app.py
+++ 02/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_ace import st_ace
+import re
 
 """
 # Advent of Code 2020 in Streamlit - 02
@@ -9,8 +11,33 @@ import streamlit as st
 """
 
 # Load the input
-problem_input = st_ace()
+problem_input = st_ace(height=150)
+show_debug_output = st.checkbox("Show debug output")
 
 """
 ## Output
 """
+
+valid_input_lines = 0
+line_expression = re.compile(
+        r"(?P<low>\d+)\-(?P<hi>\d+) (?P<char>[a-z]): (?P<pw>[a-z]+)$")
+for line_num, line in enumerate(problem_input.split("\n")):
+    match = line_expression.match(line)
+    if not match:
+        st.warning(f'Line `{line_num}`: Parse error `"{line}"`')
+        continue
+    low = int(match.group("low"))
+    hi = int(match.group("hi"))
+    char = match.group("char")
+    pw = match.group("pw")
+    char_count = len([c for c in pw if c == char])
+    if show_debug_output:
+        st.write(f'Line `{line_num}`: `"{line}"`')
+        st.write(low, hi, char, pw)
+    if low <= char_count <= hi:
+        valid_input_lines += 1
+        if show_debug_output:
+            st.success(f"Line `{line_num}` valid (`{valid_input_lines}`).")
+    elif show_debug_output:
+        st.error(f"Line `{line_num}` invalid (`{valid_input_lines}`).")
+st.write(f"In total `{valid_input_lines}` lines are valid.")
