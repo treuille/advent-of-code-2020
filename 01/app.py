be37fc37241d3bf4c53c5980a49c80ab1e36efaf
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-12-18 20:55:51 -0800

    Moved Pipfile up one level

---
+++ 01/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_ace import st_ace
+
+"""
+# Advent of Code 2020 in Streamlit - 01
+"""
+
+"""
+## Input
+"""
+
+problem_input = st_ace()
+
+st.write(
--- 01/streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-"""
-# Advent of Code 2020 in Streamlit - 01
-"""
-
-    
